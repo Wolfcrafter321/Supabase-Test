1af3f6152c2ddf012bffebf35211ee184a3b85b5
Wolfcrafter321 <Wolfcrafter321@gmail.com>
2025-04-08 12:58:43 +0900

    Add table commit test

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# Supabase Test
+
+This repository is a supabase test by wolfy.
+
+She She Ni hao wo ai nii
--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-﻿from pprint import pprint as pp
+﻿import json
 import supabase
+from supabase import create_client, Client
+from pprint import pprint as pp
 
-print(supabase)
+# read url and data
+with open("supabase-key.txt", "r", encoding="utf-8") as f:
+    supa_key = json.load(f)
+
+# NOTE: var: type ... 【:】型アノテーション
+supabase: Client = create_client(supa_key["url"], supa_key["anon-key"])
+
+# 例：データを追加
+data = {
+    "address": "192.168.0.0",
+    "note": "It's testing!"
+}
+for i in range(10):
+    res = supabase.table("Ping").insert(data).execute()
